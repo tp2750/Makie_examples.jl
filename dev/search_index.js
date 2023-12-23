d1a0108c0032f555c9b406c72dc97dcd27a7ddc0
Documenter.jl <documenter@juliadocs.github.io>
2023-12-23 09:48:28 +0000

    build based on d506d4f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Makie_examples","category":"page"},{"location":"#Makie_examples","page":"Home","title":"Makie_examples","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Makie_examples.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Makie_examples]","category":"page"}]
+}
